97f8a0e51524010970bbb1bdae13d04e784256bb
giacomotolari <tolarigiacomo@gmail.com>
2024-03-10 00:01:46 +0100

    feat: basic rest api init

--- server/urls.py
+++ server/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from .views import login, signup, test_token
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("login", login),
+    path("signup", signup),
+    path("test_token", test_token),
 ]
